013ddc500f3adb1d5d876598aa741f7c49331a8e
robinjungers <robin@jungers.fr>
2015-03-17 18:36:00 +0100

    v2.0
    Enregistrement + Login

---
+++ signin.php
@@ -0,0 +1,55 @@
+<?php
+require('bdd.php');
+require('view.php');
+
+//Début
+$titre = 'Sign Up';
+
+//Ouverture de la session.
+session_start();
+
+if( isset($_POST['identification']) )
+{
+	$mail = $_POST['mail'];
+	
+	$row = Database::get()->prepare_execute("SELECT password FROM user WHERE mail='$mail'" );
+	$password = $row[0]['password'];
+	
+	if ( $password==$_POST['password'] )
+	{
+		//Enregistrement de la session
+    $_SESSION['login'] = $_POST['mail'];
+    $_SESSION['pwd'] = $_POST['password'];
+		$message = "Vous etes bien loggé sous l'addresse: $mail";
+	}
+	else
+	{
+		//Authentification échouée
+		$message = "Erreur de connexion.";
+	}
+}
+else if( isset($_POST['deconnexion']) )
+{
+	//Suppression de la session
+	session_unset();
+	session_destroy();
+	$message = "Deconnecté.";
+}
+else 
+{
+	if (!empty($_SESSION['login']))
+	{
+		$mail = $_SESSION['login'];
+		$message = "Vous etes loggé sous l'addresse: $mail";
+	}
+	else 
+	{
+		$message = 'Veuillez saisir vos identfiants:';
+	}
+}
+
+/* Affichage */
+$vue = new View('signin.html');
+$vue->set(['TITRE' => $titre, 'MESSAGE' => $message]);
+$vue->display();
+?>
